cc8ffdf065a737f3c2770d134e175159bfc3c4ab
alagappan2578 <116268706+alagappan2578@users.noreply.github.com>
2023-07-13 19:49:11 +0530

    Day04 completed

---
+++ src/main/java/com/fssa/corejava/day04/practice/App.java
@@ -0,0 +1,17 @@
+package com.fssa.corejava.day04.practice;
+
+public class App {
+	   public static void main(String[] args) throws Exception {
+//	        RightAngledTriangle triangle = new RightAngledTriangle(3, 4, 0);
+//	        System.out.println("Area: " + triangle.area());
+//	        System.out.println("Perimeter: " + triangle.calculatePerimeter());
+//	       
+//	       
+//	        Rectangle rectangle = new Rectangle(5, 10);
+//	        
+//	        System.out.println("Area: " + rectangle.area());
+//	        System.out.println("Perimeter: " + rectangle.calculatePerimeter());
+//	      
+//	       
+	    }
+}
---
+++ src/main/java/com/fssa/corejava/day04/solved/inheritence1/Lion.java
@@ -0,0 +1,20 @@
+package com.fssa.corejava.day04.solved.inheritence1;
+
+
+public class Lion extends Animal {
+
+	public Lion(String name) {
+		super(name);		// Calling the constructor of Animal class
+		System.out.println("Calling Child Constructor");
+	}
+
+	private void roar() {
+		System.out.println("The " + getAge() + " year old " + getName() + " says: Roar!");
+	}
+
+	public static void main(String[] args) {
+		Lion simba = new Lion("Simba");
+		simba.setAge(10); // setAge() method was not defined in Lion; it is inherited from Animal
+		simba.roar();
+	}
+}
